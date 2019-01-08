29d904d462fe276f0308e433f205031337e8c019
liuyongshuai <liuyongshuai@didichuxing.com>
2019-01-08 14:02:57 +0800

    modify files

---
+++ test/md5.c
@@ -0,0 +1,23 @@
+/*************************************************************************
+ * File:	test.c
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	Thu 11 Oct 2018 07:48:54 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<unistd.h>
+#include<string.h>
+#include "../src/utils/md5.h"
+
+int main() {
+    char *str = "wendao-liuyongshuai";
+    char out[32] = {0};
+    MD5Str(str, strlen(str), out, 32);
+    printf("%s\t%s\n", str, out);
+    bzero(out, 32);
+    MD5FileStr("/Users/liuyongshuai/mycode/github/selftest/selftest.cbp", out, 32);
+    printf("%s\n", out);
+    return 0;
+}
