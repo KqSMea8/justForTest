62ad8201e60edab4f20d20625c9de32b7535581a
liuyongshuai@hotmail.com <liuyongshuai@hotmail.com>
2019-03-13 20:51:06 +0800

    modify files

---
+++ src/utils/rune_width.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+ * File:	rune_width.c
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2019年03月13日 星期三 20时50分44秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<unistd.h>
+#include<string.h>
+#include "rune_width.h"
---
+++ src/utils/rune_width.h
@@ -0,0 +1,16 @@
+/*************************************************************************
+ * File:	rune_width.h
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2019年03月13日 星期三 20时46分31秒
+ ************************************************************************/
+
+#ifndef _UTILS_RUNE_WIDTH_H_
+#define _UTILS_RUNE_WIDTH_H_
+
+#include<stdint.h>
+
+typedef struct _runeInterval{
+	int32_t first;
+	int32_t last;
+}runeInterval;
+#endif
