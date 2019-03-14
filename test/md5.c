48cd8a0ec6607531aec2d0a63956f70f9fd7e857
liuyongshuai@hotmail.com <liuyongshuai@hotmail.com>
2019-03-14 11:00:56 +0800

    modify files

--- test/md5.c
+++ test/md5.c
@@ -13,11 +13,14 @@
 #include "../src/utils/color.h"
 #include "../src/utils/rune_width.h"
 #include "../src/utils/xxhash.h"
+#include "../src/utils/from_java.h"
 
 int main() {
     char *str = "wendao-liuyongshuai";
 	unsigned long long hash = XXH64(str,strlen(str),0);
 	printf("%lld\n",hash);
+	int64_t jhash = hash_code(str,strlen(str));
+	printf("%lld\n",jhash);
     char out[32] = {0};
     MD5Str(str, strlen(str), out, 32);
     printf("%s\t%s\n", str, out);
