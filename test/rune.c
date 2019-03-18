a367e142e2ce5c700fd6c6f35f847653a75becd7
liuyongshuai <liuyongshuai@didichuxing.com>
2019-03-18 17:47:37 +0800

    s

--- test/rune.c
+++ test/rune.c
@@ -12,12 +12,18 @@
 
 void main() {
     char *str = "C/C++语言提供了几个标准库函数，可以将字符串转换为任意类型(整型、长整型、浮点型等)。";
-    int32_t buf[50] = {0};
-    convRunes(str, strlen(str), buf, 50);
+    int32_t buf[100] = {0};
+    int32_t runeNum;
+    convRunes(str, strlen(str), buf, &runeNum, 100);
     int i = 0;
     printf("\n");
-    for (; i < 50; i++) {
+    for (; i < 100; i++) {
         printf("%d\t", buf[i]);
     }
     printf("\n");
+    char *str1 = "语";
+    printf("-------%lu-----\n", strlen(str1));
+    for(i=0;i<strlen(str1);i++){
+        printf("%d\n",*(str1+i));
+    }
 }
